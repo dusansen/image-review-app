4cbc753d6cf6ac563daf88de0c98ab2898357d40
dusan.brnic <dusanbrnic92@gmail.com>
2021-12-09 22:07:33 +0100

    Add basic styled components style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import './App.css';
 import { getRandomImage } from './api/api';
 import Image from './models/Image';
+import styled from 'styled-components';
+import ButtonProps from './models/Button.props';
 
 function App() {
   const [currentImage, setCurrentImage] = useState<Image>({ id: '', url: '' });
@@ -16,11 +17,71 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <button onClick={onGetImageClickHandler}>Get Image</button>
-      {currentImage.url && <img src={currentImage.url} alt='random image' />}
-    </div>
+    <StyledWrapper>
+      <Header><h3>IMAGE APPROVAL APPLICATION</h3></Header>
+      <main>
+        <div className="approved-images"></div>
+        <div className="add-image-placeholder" />
+        {currentImage.url && <img src={currentImage.url} alt='random image' />}
+        <div className="action-buttons">
+          <Button onClick={onGetImageClickHandler}>Cancel</Button>
+          <Button buttonType='like' onClick={onGetImageClickHandler}>Like</Button>
+        </div>
+      </main>
+    </StyledWrapper>
   );
 }
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+
+    .approved-images {
+      background-color: #dcdde1;
+      width: 100%;
+      height: 2rem;
+      margin-bottom: 1rem;
+    }
+
+    .add-image-placeholder {
+      width: 40rem;
+      height: 25rem;
+      background-color: #dcdde1;
+    }
+    
+    .action-buttons {
+      display: flex;
+      flex-direction: row;
+      gap: 1.2rem;
+      margin-top: 1rem;
+    }
+  }
+`;
+
+const Button = styled.button<ButtonProps>`
+  background-color: ${props => props.buttonType === 'like' ? '#273c75' : '#353b48'};
+  color: #fff;
+  height: min-content;
+  padding: 0.8rem 1.2rem;
+  border-radius: 100px;
+  border: none;
+  cursor: pointer;
+  min-width: 6rem;
+`;
+
+const Header = styled.header`
+  text-align: left;
+  width: 100%;
+`;
 
 export default App;
