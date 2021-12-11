6446912cf3f2d76d4ae7777be71125765e46d329
dusan.brnic <dusanbrnic92@gmail.com>
2021-12-11 14:48:58 +0100

    add plus to add image placeholder

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -1,20 +1,33 @@
 import { FC } from 'react';
 import ImagePreviewProps from '../models/ImagePreview.props';
 import styled from 'styled-components';
+import plusIcon from '../assets/icons/plus.svg';
 
 export const ImagePreview: FC<ImagePreviewProps> = ({
   image: { url },
   onImageClick
 }) => {
   return !url
-    ? <AddImagePlaceholder onClick={onImageClick} />
+    ? (
+      <AddImagePlaceholder onClick={onImageClick}>
+        <img src={plusIcon} alt='add image' />
+      </AddImagePlaceholder>
+    )
     : <Image src={url} alt='photo' />
 };
 
 const AddImagePlaceholder = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 40rem;
   height: 25rem;
   background-color: #dcdde1;
+
+  img {
+    width: 10rem;
+    height: 10rem;
+  }
 `;
 
 const Image = styled.img`
