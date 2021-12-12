43ad30f6b96a503cfbe73bc23093e7abc1ac30fc
dusan.brnic <dusanbrnic92@gmail.com>
2021-12-12 01:12:27 +0100

    set number of images per page when window size changes

--- src/components/ApprovedImages.tsx
+++ src/components/ApprovedImages.tsx
@@ -1,17 +1,33 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ApprovedImagesProps from '../models/ApprovedImages.props';
 import Carousel from 'react-elastic-carousel';
 import { ImageThumbnail } from './ImageThumbnail';
 import { Header } from './styled/Header';
+import { useWindowSize } from '../hooks/useWindowSize';
+
+const INITIAL_NUMBER_OF_IMAGES_TO_SHOW = 6;
 
 export const ApprovedImages: FC<ApprovedImagesProps> = ({ images }) => {
+  const [numberOfImagesToShow, setNumberOfImagesToShow] = useState(INITIAL_NUMBER_OF_IMAGES_TO_SHOW);
+  const size = useWindowSize();
+
+  useEffect(() => {
+    setNumberOfImagesToShow(Math.floor(size.width / 200));
+  }, [size]);
+
   return (
     <StyledWrapper>
       <Header><h3>APPROVED IMAGES ({images.length})</h3></Header>
-      <Carousel pagination={false} isRTL={false} itemsToShow={6}>
-        {images.map(image => <ImageThumbnail key={image.id} image={image} />)}
-      </Carousel>
+      {
+        images.length
+          ? (
+            <Carousel pagination={false} isRTL={false} itemsToShow={numberOfImagesToShow}>
+              {images.map(image => <ImageThumbnail key={image.id} image={image} />)}
+            </Carousel>
+          )
+          : null
+      }
     </StyledWrapper>
   );
 }
